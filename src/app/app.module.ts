1430e58fdda5dca2659c93585ad808d13e36f9f3
cchura94 <cchura.cpc@gmail.com>
2022-07-12 18:50:33 -0400

    Estructura modulos y rutas + componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IncioComponent } from './incio/incio.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IncioComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
